08209fa93b0d4914531944be4e21d7e160b45646
PerviousSkate <139131296+PerviousSkate@users.noreply.github.com>
2023-09-08 13:25:39 -0300

    cuarto-punto.c

--- cuarto-punto.c
+++ cuarto-punto.c
@@ -2,37 +2,77 @@
 #include <math.h>
 #define PI 3.14
 
-    float frecuencia;
-    float real;
-    float imag;
-    float W;
-    float inductor;
-    float capacitor;
-    float resistencia;
+//declaracion de funciones//
+float calculo_omega(float frecuencia);
+float pedido_de_real();
+float pedido_de_imaginario();
+float pedido_de_frecuencia();
+float calculo(float, float);
+void resultado(float, float, float);
+
 int main()
 {
-    printf("Por favor ingrese la parte real de su impedancia\n");
-    scanf("%f", &real);
-    printf("Por favor ingrese la parte imaginaria de su impedancia\n");
-    scanf("%f", &imag);
-    printf("Por ultimo por favor ingrese la frecuencia\n");
-    scanf("%f", &frecuencia);
-    W=2*PI*frecuencia;
-    if(frecuencia!=0){
-        if(imag!=0){
-            if(imag<0){
-                capacitor=-1/(imag*W);
-                printf("Su reactancia es principalmente capacitiva, el posible capacitor valdra %f",capacitor);
-            }else{
-                inductor=imag/W;
-                printf("Su reactancia es principalmente inductiva, el posible inductor valdra %f",inductor);
-            }
-        }
-    }else{
-        resistencia=real;
-        printf("Su resistencia es de:%f",resistencia);
-    }
-    
+    //declaracion de variables//
+    float frecuencia;
+    float real,imag;
+    float W;
+    float componente;
+
+    real=pedido_de_real();
+    imag=pedido_de_imaginario();
+    frecuencia=pedido_de_frecuencia();
+
+    W=calculo_omega(frecuencia);
+    componente=calculo(imag, W);
+    resultado(componente, real, imag);
 
     return 0;
+}
+
+float calculo_omega(float frecuencia){
+    float W;
+    W=2*PI*frecuencia;
+    return W;
+}
+
+float pedido_de_real(){
+    float real;
+    printf("Por favor ingrese la parte real de su impedancia\n");
+    scanf("%f", &real);
+    return real;
+}
+
+float pedido_de_imaginario(){
+    float imag;
+    printf("Por favor ingrese la parte imaginaria de su impedancia\n");
+    scanf("%f", &imag);
+    return imag;
+}
+
+float pedido_de_frecuencia(){
+    float frecuencia;
+    printf("Por ultimo por favor ingrese la frecuencia\n");
+    scanf("%f", &frecuencia);
+    return frecuencia;
+}
+
+float calculo(float imag,float W){
+    float componente=0;
+    if(imag<0){
+        componente=-1/(imag*W);
+    }
+    if(imag>0){
+        componente=imag/W;
+    }
+    return componente;
+}
+
+void resultado(float componente,float real,float imag){
+    if(imag>0){
+        printf("el valor del inductor es: %fH\n", componente);
+    }
+    if(imag<0){
+        printf("el valor del capacitor es: %fF\n", componente);
+    }
+    printf("el valor de la resistencia es:%fΩ\n", real);
 }
